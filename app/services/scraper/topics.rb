97fee23316cc9eeb8875ef4e03bdd1bc79e497cc
Roberto Zahuis <zahuis314@gmail.com>
2022-01-20 19:43:06 -0500

    Change topic tags fetched data

--- app/services/scraper/topics.rb
+++ app/services/scraper/topics.rb
@@ -7,8 +7,8 @@ module Scraper
                 questionTopicTags {
                     edges {
                         node {
+                            id
                             name
-                            translatedName
                             slug
                         }
                     }
