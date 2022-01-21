d2ab44786fe013a7bbda08911b9229fc3f689045
Roberto Zahuis <zahuis314@gmail.com>
2022-01-20 20:34:46 -0500

    Seed database with topic tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+topic_tags = Scraper::TopicTags.create_topic_tags
