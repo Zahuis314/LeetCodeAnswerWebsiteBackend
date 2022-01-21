7afcdd6aad9d4d48f78a6d720b1e8e6bf87ed707
Roberto Zahuis <zahuis314@gmail.com>
2022-01-21 18:45:21 -0500

    Modify Problem model

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -4,4 +4,6 @@ class Problem < ApplicationRecord
     Medium: 2,
     Hard: 3
   }, _prefix: true
+  validates_presence_of :title, :title_slug, :is_paid_only, :question_id, :question_frontend_id
+  validates_uniqueness_of :title, :title_slug, :question_id, :question_frontend_id
 end
