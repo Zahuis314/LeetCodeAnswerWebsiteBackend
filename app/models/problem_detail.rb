eadcb14da5b2566a7502d488c594fd088da97ace
Roberto Zahuis <zahuis314@gmail.com>
2022-01-24 17:47:22 -0500

    Fix problem detail creation refactor

--- app/models/problem_detail.rb
+++ app/models/problem_detail.rb
@@ -7,11 +7,11 @@ class ProblemDetail < ApplicationRecord
     value = Problem.find(value) if (value.is_a? String)
     problem_detail = value.problem_detail
     if problem_detail.nil?
-      problem_detail = Scraper::Problems.get_problem_details(value.slug)[:data]
-      similar_questions_gql = details.delete :similarQuestions
+      problem_detail_gql = Scraper::Problems.get_problem_details(value.slug)[:data]
+      similar_questions = problem_detail_gql.delete :similarQuestions
 
-      value.create_problem_detail details.to_hash
-      similar_questions_ids = Problem.where(slug: similar_questions_gql.map(&:titleSlug)).map(&:id)
+      problem_detail = value.create_problem_detail problem_detail_gql.to_hash
+      similar_questions_ids = Problem.where(slug: similar_questions.map(&:titleSlug)).map(&:id)
       value.similarity_ids=similar_questions_ids
     end
     problem_detail
