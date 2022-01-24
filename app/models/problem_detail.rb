3beeec5258e0ec2fa151fdbb9d911d0589cdcf94
Roberto Zahuis <zahuis314@gmail.com>
2022-01-23 21:21:31 -0500

    Modify findOrCreate in ProblemDetail Model to retrieve and save instance

--- app/models/problem_detail.rb
+++ app/models/problem_detail.rb
@@ -1,3 +1,19 @@
 class ProblemDetail < ApplicationRecord
   belongs_to :problem
+  serialize :metaData
+  serialize :hints
+
+  def self.findOrCreate(value)
+    value = Problem.find(value) if (value.is_a? String)
+    problem_detail = value.problem_detail
+    if problem_detail.nil?
+      problem_detail = Scraper::Problems.get_problem_details(value.slug)[:data]
+      similar_questions_gql = details.delete :similarQuestions
+
+      value.create_problem_detail details.to_hash
+      similar_questions_ids = Problem.where(slug: similar_questions_gql.map(&:titleSlug)).map(&:id)
+      value.similarity_ids=similar_questions_ids
+    end
+    problem_detail
+  end
 end
